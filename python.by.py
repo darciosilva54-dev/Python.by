ef7cc020fe770c6a64aad667394afb54bb4dda24
Darcio silva de castro <darciosilva54@gmail.com>
2026-02-12 19:33:41 -0300

    Add files via upload

---
+++ python.by.py
@@ -0,0 +1,21 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print('olá, mundo!')
+olá, mundo!
+>>> print('7+4')
+7+4
+>>> print(7+4)
+11
+>>> print('7'+'4')
+74
+>>> 7+4
+11
+>>> '7'+'4'
+'74'
+>>> print('olá', 5)
+olá 5
+>>> nome='guanabara'
+>>> idade=26
+>>> peso=60
+>>> print(nome,idade,peso)
+guanabara 26 60
